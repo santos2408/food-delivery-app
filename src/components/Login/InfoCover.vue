7577c519cb09759e620dcf66432ae059920a89ad
Roger Santos <roger.santos36@gmail.com>
2023-07-24 20:20:21 -0300

    feat: create infoCover component to loginView component

---
+++ src/components/Login/InfoCover.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hidden flex-1 justify-center bg-brand-purple-3 md:block">
+    <div class="h-full md:flex md:flex-col xl:justify-center xl:gap-10 2xl:gap-5">
+      <div class="relative h-[75%] overflow-hidden xl:h-auto">
+        <img
+          :src="getCoverImage"
+          class="absolute left-4 mx-auto w-[655px] max-w-none xl:relative xl:w-[920px] xl:max-w-full"
+          alt="Login cover image"
+        />
+      </div>
+      <login-swiper />
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginSwiper from "@/components/Login/LoginSwiper.vue";
+
+export default {
+  name: "InfoCover",
+  components: {
+    LoginSwiper,
+  },
+  props: {
+    screenWidth: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    getCoverImage() {
+      const isAMobileDevice = this.screenWidth >= 768 && this.screenWidth <= 1280;
+      console.log(this.screenWidth);
+      return isAMobileDevice
+        ? new URL(`@/assets/Login/login-cover-tablet.png`, import.meta.url).href
+        : new URL(`@/assets/Login/login-cover-desktop.png`, import.meta.url).href;
+    },
+  },
+};
+</script>
