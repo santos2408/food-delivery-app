ec9c10e7a784228f11ab3b1304a468b205bd47a1
Roger Santos <roger.santos36@gmail.com>
2024-01-16 12:32:48 -0300

    vercel config

--- src/stores/restaurants.js
+++ src/stores/restaurants.js
@@ -1,32 +1,47 @@
 import { defineStore } from "pinia";
 
-export const FETCH_RESTAURANTS = "FETCH_RESTAURANTS";
-export const FILTERED_RESTAURANTS = "FILTERED_RESTAURANTS";
+import { useUserStore } from "@/stores/user";
 
 import getRestaurants from "@/api/getRestaurants";
 
+export const FETCH_RESTAURANTS = "FETCH_RESTAURANTS";
+export const FILTERED_RESTAURANTS = "FILTERED_RESTAURANTS";
+export const CLEAR_RESTAURANTS = "CLEAR_RESTAURANTS";
+export const INCLUDE_RESTAURANTS_BY_TYPE = "INCLUDE_RESTAURANTS_BY_TYPE";
+
 export const useRestaurantsStore = defineStore("restaurants", {
   state: () => ({
-    filteredRestaurants: {},
+    restaurants: { data: [], hasNext: null },
   }),
   getters: {
+    [INCLUDE_RESTAURANTS_BY_TYPE]: () => (restaurant) => {
+      const userStore = useUserStore();
+      console.log(userStore.selectedRestaurantTypes);
+      if (userStore.selectedRestaurantTypes.length === 0) {
+        return true;
+      }
+      return userStore.selectedRestaurantTypes.includes(restaurant);
+    },
     [FILTERED_RESTAURANTS]: (state) => {
       return state.restaurants;
+      // this.INCLUDE_RESTAURANTS_BY_TYPE("japonesa");
+      // return state.restaurants.data.filter((restaurant) => {
+      //   return this.INCLUDE_RESTAURANTS_BY_TYPE(restaurant);
+      // });
     },
   },
   actions: {
-    async [FETCH_RESTAURANTS](currentPage) {
-      const restaurants = await getRestaurants(currentPage);
-
-      if (!this.restaurants.data) {
-        this.restaurants = { ...restaurants };
-        return;
-      }
+    async [FETCH_RESTAURANTS](currentPage, category) {
+      const restaurants = await getRestaurants(currentPage, category);
 
       this.restaurants = {
         data: [...this.restaurants.data, ...restaurants.data],
         hasNext: restaurants.hasNext,
       };
+    },
+
+    [CLEAR_RESTAURANTS]() {
+      this.restaurants = { data: [], hasNext: null };
     },
   },
 });
